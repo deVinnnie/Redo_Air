107cdd5a959f69b0778d8efa948ff487f8f86f9c
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2016-09-07 22:15:28 +0200

    Add some more unit tests for TravelClass

--- src/test/java/com/realdolmen/air/domain/TravelClassTest.java
+++ src/test/java/com/realdolmen/air/domain/TravelClassTest.java
@@ -4,20 +4,73 @@ import com.realdolmen.air.util.Asserter;
 import org.junit.Before;
 import org.junit.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.math.BigDecimal;
+import java.util.Set;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class TravelClassTest {
     private TravelClass travelClass;
 
     private Asserter asserter = new Asserter();
+    private Validator validator;
 
     @Before
     public void setUp(){
         this.travelClass = new TravelClass("Business", 5, new BigDecimal("100.0"), null);
         this.travelClass.setMargin(new BigDecimal("1.05"));
+
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        this.validator = factory.getValidator();
     }
+
+    @Test
+    public void basePriceWithMoreThanTwoDecimalPlacesShouldBeInvalid(){
+        travelClass.setBasePrice(
+                new BigDecimal("3.141592")
+        );
+        
+
+        Set<ConstraintViolation<TravelClass>> violations = validator.validate(travelClass);
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void basePriceWitTwoDecimalPlacesShouldBeValid(){
+        travelClass.setBasePrice(
+                new BigDecimal("3.14")
+        );
+
+        Set<ConstraintViolation<TravelClass>> violations = validator.validate(travelClass);
+        assertEquals(0, violations.size());
+    }
+
+    @Test
+    public void overriddenPriceWithMoreThanTwoDecimalPlacesShouldBeInvalid(){
+        travelClass.setBasePrice(
+                new BigDecimal("3.141592")
+        );
+
+
+        Set<ConstraintViolation<TravelClass>> violations = validator.validate(travelClass);
+        assertEquals(1, violations.size());
+    }
+
+    @Test
+    public void overriddenPriceWitTwoDecimalPlacesShouldBeValid(){
+        travelClass.setOverriddenPrice(
+                new BigDecimal("3.14")
+        );
+
+        Set<ConstraintViolation<TravelClass>> violations = validator.validate(travelClass);
+        assertEquals(0, violations.size());
+    }
+
 
     @Test
     public void test_getEndUserPrice_WithNormalBasePrice_GivesCorrectResult(){
